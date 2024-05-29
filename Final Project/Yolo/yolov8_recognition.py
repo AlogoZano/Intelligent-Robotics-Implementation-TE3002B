26ca391d2a9aceb05b22883f5fa8cc71ff0bad2f
AlogoZano <160699916+AlogoZano@users.noreply.github.com>
2024-05-28 19:24:20 -0600

    recognition

---
+++ Final Project/Yolo/yolov8_recognition.py
@@ -0,0 +1,86 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from cv_bridge import CvBridge
+from ultralytics import YOLO
+import numpy as np
+import cv2
+
+from yolo_msg.msg import InferenceResult
+from yolo_msg.msg import Yolov8Inference
+
+from cv_bridge import CvBridgeError
+
+class YoloInference(Node):
+    def __init__(self):
+        super().__init__('yolo_node')
+        self.model = YOLO('/home/alogo/ros2_ws/src/yolov8_ros2/yolov8_ros2/last.pt')
+        self.yolo_msg = Yolov8Inference()
+        self.img = np.ndarray((720, 1280, 3))
+        self.img_reduced = np.ndarray((320, 1280, 3))
+        self.bridge = CvBridge()
+        self.valid_img = False  # Inicialización de valid_img
+
+        self.sub = self.create_subscription(Image, 'video_source/raw', self.camera_callback, 10)
+        self.yolo_pub = self.create_publisher(Yolov8Inference, '/Yolov8_inference', 10)
+        self.publihser_signal_class = self.create_publisher(String, '/class_name', 10)
+        self.yolo_img_pub = self.create_publisher(Image, '/inference_result', 10)
+
+        self.signal_class_msg = String()
+
+        timer_period = 1/20
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def camera_callback(self, msg):
+        try:
+            self.img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            self.img = cv2.rotate(self.img, cv2.ROTATE_180)
+            self.img_reduced = self.img[200:520, 1:1280]
+            self.valid_img = True
+        except CvBridgeError as e:
+            self.get_logger().info(f'Failed to get an image: {e}')
+            self.valid_img = False
+
+    def timer_callback(self):
+        if self.valid_img:
+            results = self.model(self.img_reduced)
+            self.yolo_msg.header.frame_id = 'inference'
+            self.yolo_msg.header.stamp = self.get_clock().now().to_msg()
+
+            for r in results:
+                boxes = r.boxes
+                for box in boxes:
+                    self.inference_result = InferenceResult()
+                    b = box.xyxy[0].to('cpu').detach().numpy().copy()
+                    c = box.cls
+                    conf = box.conf
+                    print(conf)
+                    self.inference_result.class_name = self.model.names[int(c)]
+                    # self.inference_result.top = int(b[0])
+                    # self.inference_result.left = int(b[1])
+                    # self.inference_result.bottom = int(b[2])
+                    # self.inference_result.right = int(b[3])
+                    # self.yolo_msg.yolov8_inference.append(self.inference_result)
+                    self.signal_class_msg.data = self.inference_result.class_name
+            
+            #frame = results[0].plot()
+
+            
+            # Convert float64 image to uint8
+            #frame_uint8 = (frame * 255).astype(np.uint8)
+            
+            #self.yolo_img_pub.publish(self.bridge.cv2_to_imgmsg(frame_uint8, encoding='bgr8'))
+            self.yolo_pub.publish(self.yolo_msg)
+            self.publihser_signal_class.publish(self.signal_class_msg)
+            self.yolo_msg.yolov8_inference.clear()
+
+def main(args=None):
+    rclpy.init(args=args)
+    y_i = YoloInference()
+    rclpy.spin(y_i)
+    y_i.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
